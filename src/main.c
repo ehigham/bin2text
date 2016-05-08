8214301374d8b290d97ba922d33eb2f3324e43d3
Thomas Legris <legris.thomas@gmail.com>
2016-05-08 14:31:12 +0100

    Add traits mechanism

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 
 #include "utils/dispatcher.h"
 #include "utils/optional.h"
+#include "utils/traits.h"
 #include <stdint.h>
 #include <stdio.h>
+#include <stdbool.h>
 
 struct bar
 {
@@ -21,6 +23,8 @@ struct bar2
 void get_dummy1(struct bar a) {printf("dummy1\n");} 
 void get_dummy2(struct bar2 b) {printf("dummy2\n");} 
 
+bool trait_Comparable_struct_bar_cmp() { return true;}
+
 int main(int argc, const char *argv[])
 {
   struct bar myBar;
@@ -29,6 +33,7 @@ int main(int argc, const char *argv[])
   static_dispatch(myBar, get_dummy);
   static_dispatch(myBar2, get_dummy);
 
+  traits_struct_dispatch(Comparable, bar,cmp);
 
   
   return 0;
