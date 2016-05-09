00bd5054311cafcdb1ba109f6539f9f10c257897
Thomas Legris <legris.thomas@gmail.com>
2016-05-09 23:36:34 +0100

    Fix & add check to Heap structure

--- utils/heap.c
+++ utils/heap.c
@@ -4,6 +4,34 @@
 #define swap(a, b) {value_type tmp = a; a = b; b = tmp;}
  
 // ============================================================================
+inline void helper_push_back(struct heap *this, const value_type content)
+{
+  assert(this->size+1 <= this->allocated_size);
+  this->data[this->size++] = content;                                          
+}
+
+// ============================================================================
+inline void helper_pop_back(struct heap *this)
+{
+  assert(this->size != 0);
+  --this->size;
+}
+
+// ============================================================================
+inline value_type helper_back(struct heap *this)
+{
+  assert(this->size != 0);
+  return this->data[this->size-1];
+}
+
+// ============================================================================
+inline value_type helper_front(struct heap *this)
+{
+  assert(this->size != 0);
+  return this->data[0];
+}
+
+// ============================================================================
 void heap_init(struct heap *this, const size_t N)
 {
   this->size=0;
@@ -12,16 +40,71 @@ void heap_init(struct heap *this, const size_t N)
 }
 
 // ============================================================================
-void heap_delete(struct heap *this, const size_t N)
+void heap_delete(struct heap *this)
 {
   assert(this->data != NULL);
   free(this->data);
 }
 
 // ============================================================================
-void max_insert(struct heap *this, const value_type content)                      
+void heap_max_insert(struct heap *this, const value_type content)                      
 {                                                                              
-  this->data[this->size++] = content;                                          
+  helper_push_back(this, content);
+  size_t current_id = this->size-1;                                            
+  size_t parent_id  = current_id/2;                                            
+                                                                               
+  while(current_id != 0)                                                       
+    if(this->data[current_id] > this->data[parent_id])
+    {                                                                          
+      swap(this->data[parent_id], this->data[current_id]);                     
+      
+      current_id = parent_id;                                                  
+      parent_id = current_id/2;                                                
+    }                                                                          
+    else                                                                       
+      break;                                                                   
+}                                                                              
+
+// ============================================================================
+value_type heap_max_peek(struct heap *this)
+{
+  assert(this->data);
+  assert(this->size != 0);
+
+  value_type res = helper_front(this);
+  this->data[0] = helper_back(this);
+  helper_pop_back(this);
+
+  size_t current_id=0;
+
+  while(current_id < this->size/2)
+  {
+    size_t left_chil_id  = 2*current_id + 1;
+    size_t right_chil_id = 2*current_id + 2;
+
+    size_t to_swap = left_chil_id;
+
+    // Transitivity trick
+    if(this->data[left_chil_id] < this->data[right_chil_id])
+      to_swap = right_chil_id;
+
+    if(this->data[to_swap] > this->data[current_id]) 
+    {
+      swap(this->data[current_id], this->data[to_swap]);
+      current_id = to_swap;
+    }
+    else break;
+  }
+
+  return res;
+}
+
+// ============================================================================
+void heap_min_insert(struct heap *this, const value_type content)                      
+{                                                                              
+  assert(this->data);
+
+  helper_push_back(this, content);
   size_t current_id = this->size-1;                                            
   size_t parent_id  = current_id/2;                                            
                                                                                
@@ -37,90 +120,34 @@ void max_insert(struct heap *this, const value_type content)
 }                                                                              
 
 // ============================================================================
-value_type max_peek(struct heap *this)
+value_type heap_min_peek(struct heap *this)
 {
   assert(this->data);
   assert(this->size != 0);
 
-  value_type res = this->data[0];
-  this->data[0] = this->data[this->size-1];
-  --this->size;
+  value_type res = helper_front(this);
+  this->data[0] = helper_back(this);
+  helper_pop_back(this);
 
-  size_t current_id= 0;
-  size_t left_chil_id = 2*current_id + 1;
-  size_t right_chil_id = 2*current_id + 2;
+  size_t current_id=0;
 
-  while(current_id <= this->size/2)
-    if(this->data[current_id] >= this->data[left_chil_id])
+  while(current_id < this->size/2)
+  {
+    size_t left_chil_id  = 2*current_id + 1;
+    size_t right_chil_id = 2*current_id + 2;
+
+    size_t to_swap = left_chil_id;
+
+    // Transitivity trick
+    if(this->data[left_chil_id] > this->data[right_chil_id])
+      to_swap = right_chil_id;
+
+    if(this->data[to_swap] < this->data[current_id]) 
     {
-      swap(this->data[current_id], this->data[left_chil_id]);
-      current_id = left_chil_id;
-      left_chil_id = 2*current_id + 1;
-      right_chil_id = 2*current_id + 2;
-    }
-    else if(this->data[current_id] >= this->data[right_chil_id])
-    {
-      swap(this->data[current_id], this->data[right_chil_id]);
-      current_id = right_chil_id;
-      left_chil_id = 2*current_id + 1;
-      right_chil_id = 2*current_id + 2;
-
+      swap(this->data[current_id], this->data[to_swap]);
+      current_id = to_swap;
     }
     else break;
-
-  return res;
-}
-
-// ============================================================================
-void min_insert(struct heap *this, const value_type content)                      
-{                                                                              
-  assert(this->data);
-  this->data[this->size++] = content;                                          
-  size_t current_id = this->size-1;                                            
-  size_t parent_id  = current_id/2;                                            
-                                                                               
-  while(current_id != 0)                                                       
-    if(this->data[current_id] > this->data[parent_id])
-    {                                                                          
-      swap(this->data[parent_id], this->data[current_id]);                     
-      current_id = parent_id;                                                  
-      parent_id = current_id/2;                                                
-    }                                                                          
-    else                                                                       
-      break;                                                                   
-}                                                                              
-
-// ============================================================================
-value_type min_peek(struct heap *this)
-{
-  assert(this->data);
-  assert(this->size != 0);
-
-  value_type res = this->data[0];
-  this->data[0] = this->data[this->size-1];
-  --this->size;
-
-  size_t current_id= 0;
-  size_t left_chil_id = 2*current_id + 1;
-  size_t right_chil_id = 2*current_id + 2;
-
-  while(current_id <= this->size/2)
-    if(this->data[current_id] <= this->data[left_chil_id])
-    {
-      swap(this->data[current_id], this->data[left_chil_id]);
-      current_id = left_chil_id;
-      left_chil_id = 2*current_id + 1;
-      right_chil_id = 2*current_id + 2;
-    }
-    else if(this->data[current_id] <= this->data[right_chil_id])
-    {
-      swap(this->data[current_id], this->data[right_chil_id]);
-      current_id = right_chil_id;
-      left_chil_id = 2*current_id + 1;
-      right_chil_id = 2*current_id + 2;
-
-    }
-    else break;
-
+  }
   return res;
 }
--- utils/heap.h
+++ utils/heap.h
@@ -9,12 +9,27 @@
 
 #define value_type uint32_t
 
+// Binary heap structure
 struct heap
 {
   size_t size;
   size_t allocated_size;
   value_type * data;
 };
+
+// Heap ctro & dtor
+void heap_init(struct heap *this, const size_t N);
+void heap_delete(struct heap *this);
+
+// Max heap operations
+void heap_max_insert(struct heap *this, const value_type newkey);
+value_type heap_max_peek(struct heap *this);
+
+// Min Heap operations
+void heap_min_insert(struct heap *this, const value_type newkey);
+value_type heap_min_peek(struct heap *this);
+
+
 
 #undef value_type
 
