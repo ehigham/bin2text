7753a635eb0e356a78780d891b40eb999263cbf7
ehigham <edhigham@gmail.com>
2016-05-12 20:58:36 +0100

    added string resource file for filenames

---
+++ src/resource/strings.h
@@ -0,0 +1,28 @@
+/*
+ *    This file is part of bin2text.
+ *
+ *    bin2text is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at youroption) any later version.
+ *
+ *    bin2text is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with bin2text.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    (C) 2016, Thomas Legris, Edmund Higham
+**/
+
+struct output_files
+{
+    static const char[] out1_name = "out1.txt";
+    static const char[] out2_name = "out2.txt";
+    static const char[] out3_name = "out2.txt";
+    static const char[] out4_name = "out2.txt";
+    static const char[] out5_name = "out2.txt"; 
+};
+
